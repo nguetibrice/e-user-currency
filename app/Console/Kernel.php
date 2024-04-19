9c4e7b59ed73a96221d1b6363f72de931e4e4f7c
Hermann <wambahermann084@gmail.com>
2024-04-19 17:25:35 +0100

    add job scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,10 @@
 
 namespace App\Console;
 
+use App\Jobs\ExchangeRateJob;
+use App\Services\Contracts\ExchangeRateApiContract;
+use App\Services\Contracts\ExchangeRateContract;
+use App\Services\Contracts\ICurrencyService;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,6 +17,19 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $apiExchangeRatesService = app(ExchangeRateApiContract::class);
+        $exchangeRateService = app(ExchangeRateContract::class);
+        $currencyService = app(ICurrencyService::class);
+        $schedule
+        ->job(new ExchangeRateJob(
+            $apiExchangeRatesService,
+            $exchangeRateService,
+            $currencyService,
+            env("DEFAULT_CURRENCY")
+        ))
+        ->withoutOverlapping()
+        ->everySixHours()
+        ;
     }
 
     /**
